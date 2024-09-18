4850a1e1d10986c7926181cf2dc7703cce244f1d
aichouch <aychaa.benmakhlouf@gmail.com>
2024-09-18 20:05:16 +0200

    add ignore file

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
+   # path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
